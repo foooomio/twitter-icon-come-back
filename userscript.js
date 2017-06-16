37a4a29228aaa4e5b45e172ef70e7f1b0d65bc64
foooomio <foooomio@gmail.com>
2017-06-16 22:46:53 +0900

    Refactor

--- userscript.js
+++ userscript.js
@@ -13,10 +13,9 @@
 (function() {
     document.body.className = document.body.className.replace('edge-design');
     new MutationObserver(function(mutations) {
-        if (!mutations.some(function(mutation) {
+        if (mutations.some(function(mutation) {
             return mutation.oldValue.indexOf('edge-design') !== -1;
-        })) return;
-        document.body.className = document.body.className.replace('edge-design');
+        })) document.body.className = document.body.className.replace('edge-design');
     }).observe(
         document.body,
         { attributes: true, attributeOldValue: true, attributeFilter: ['class'] }
